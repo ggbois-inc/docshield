c444d9fd4b171297f41d8c7a8a8215cbe30bec78
Parag003 <goyalparag54@gmail.com>
2023-06-26 15:22:51 +0530

    Final ui changes

--- components/sidebar.js
+++ components/sidebar.js
@@ -10,10 +10,10 @@ import SideButton from "./sidebutton";
 export default function Sidebar() {
   const { account } = useContext(AppContext)
   return (
-    <div className="flex flex-col basis-1/12 lg:basis-[20%] p-3 gap-4  fixed h-full bg-black pb-5">
+    <div className="flex flex-col basis-1/12 lg:basis-[20%] p-3 gap-4  fixed h-full bg-black pb-5 ">
       <div className="brand-logo w-16 h-16 flex flex-row items-center space-x-3">
-        <img src="/docshield.png" class="ml-4"></img>
-        <div class="text-white text-3xl mr-10 font-bold hidden lg:block ">
+        <img src="/docshield.png" class=" flex-col ml-4"></img>
+        <div class="text-white text-3xl mr-10 font-bold hidden lg:block flex-col ">
           DocShield
         </div>
       </div>
